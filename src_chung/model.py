bd6049813a6b598d1ec40634cf7785cc7137eaf8
phuocchung123 <chungnvp197@gmail.com>
2024-04-08 18:46:57 +0700

    modify to run on kaggle (18)

--- src_chung/model.py
+++ src_chung/model.py
@@ -136,8 +136,8 @@ class reactionMPNN(nn.Module):
         )
 
         # Cross-Attention Module
-        self.rea_attention_pro = EncoderLayer(128, 0.1, 0.1, 2)  # 注意力机制
-        self.pro_attention_rea = EncoderLayer(128, 0.1, 0.1, 2)
+        self.rea_attention_pro = EncoderLayer(1024, 0.1, 0.1, 2)  # 注意力机制
+        self.pro_attention_rea = EncoderLayer(1024, 0.1, 0.1, 2)
 
     def forward(self, rmols, pmols):
         r_graph_feats = torch.sum(torch.stack([self.mpnn(mol) for mol in rmols]), 0)
