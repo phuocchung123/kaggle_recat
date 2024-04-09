4753dd13f9a18b34cf5deab4c9e6194990f26206
phuocchung123 <chungnvp197@gmail.com>
2024-04-09 20:48:51 +0700

    modify to run on lab kaggle (57)

--- src_chung/model.py
+++ src_chung/model.py
@@ -33,7 +33,7 @@ class GIN(nn.Module):
         self,
         node_in_feats,
         edge_in_feats,
-        depth=2,
+        depth=3,
         node_hid_feats=300,
         readout_feats=1024,
         dr=0.1,
