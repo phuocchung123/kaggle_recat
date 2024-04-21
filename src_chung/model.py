6c499715edf149cea4627986ffd04869489d2feb
phuocchung123 <chungnvp197@gmail.com>
2024-04-21 20:44:43 +0700

    update

--- src_chung/model.py
+++ src_chung/model.py
@@ -181,7 +181,7 @@ def training(
     print('rmol_max_cnt:', rmol_max_cnt, '\n pmol_max_cnt:', pmol_max_cnt)
 
     loss_fn = nn.CrossEntropyLoss()
-    n_epochs = 2
+    n_epochs = 50
     optimizer = Adam(net.parameters(), lr=5e-4, weight_decay=1e-5)
 
 
