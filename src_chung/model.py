7640af045bbfb111b1d473f819615d728f3df98d
phuocchung123 <chungnvp197@gmail.com>
2024-05-15 11:45:07 +0700

    update

--- src_chung/model.py
+++ src_chung/model.py
@@ -118,7 +118,7 @@ class reactionMPNN(nn.Module):
         pretrained_model_path=None,
         readout_feats=300,
         predict_hidden_feats=512,
-        prob_dropout=0.1,
+        prob_dropout=0.4,
         cuda=torch.device('cuda:0')
     ):
         super(reactionMPNN, self).__init__()
@@ -204,9 +204,9 @@ class reactionMPNN(nn.Module):
             # reactants,_=self.rea_attention_pro(reactants, reactants)
             # products,_=self.pro_attention_rea(products,products)
 
-            # reactants_noncross=reactants
-            # reactants,att_r=self.rea_attention_pro(reactants, products)
-            # products,att_p=self.pro_attention_rea(products, reactants_noncross)
+            reactants_noncross=reactants
+            reactants,att_r=self.rea_attention_pro(reactants, products)
+            products,att_p=self.pro_attention_rea(products, reactants_noncross)
             reactants=torch.sum(reactants,0).unsqueeze(0)
             products= torch.sum(products,0).unsqueeze(0)
 
@@ -267,7 +267,7 @@ def training(
     # print('rmol_max_cnt:', rmol_max_cnt, '\n pmol_max_cnt:', pmol_max_cnt)
 
     loss_fn = nn.CrossEntropyLoss()
-    n_epochs = 100
+    n_epochs = 30
     optimizer = Adam(net.parameters(), lr=5e-4, weight_decay=1e-5)
 
 
