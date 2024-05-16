6a067df07bbff4b2aabda3f73c9b6f7ab6f158e5
phuocchung123 <chungnvp197@gmail.com>
2024-05-16 17:47:28 +0700

    update data_non_reagent

--- src_chung/model.py
+++ src_chung/model.py
@@ -198,8 +198,8 @@ class reactionMPNN(nn.Module):
             start_list_p=end_list_p
 
             reactants_noncross=reactants
-            reactants=self.rea_attention_pro(reactants, products)
-            products=self.pro_attention_rea(products, reactants_noncross)
+            reactants,att_r=self.rea_attention_pro(reactants, products)
+            products,att_p=self.pro_attention_rea(products, reactants_noncross)
             reactants=torch.sum(reactants,0).unsqueeze(0)
             products= torch.sum(products,0).unsqueeze(0)
 
