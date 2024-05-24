50a993e1d26ca98713922aa7320c15f9c60874bd
phuocchung123 <chungnvp197@gmail.com>
2024-05-25 00:45:48 +0700

    modify SubLayer.py because self attention for each reaction not for batch

--- main_finetune.py
+++ main_finetune.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
         os.makedirs("/kaggle/working/sample/data_chung/model/finetuned/")
 
     start_point=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print('multi_head=1,weights for combination R,P and reagents=0.7,epoch=50,mean instead of sum')
+    print('Selfattention,multi_head=8, epoch=20, dim_inner=2048,d_k=64,d_v=64')
 
     finetune(args)
 
--- src_chung/SubLayers.py
+++ src_chung/SubLayers.py
@@ -30,7 +30,8 @@ class MultiHeadAttention(nn.Module):
     def forward(self, q, k, v, mask=None):
 
         d_k, d_v, n_head = self.d_k, self.d_v, self.n_head
-        sz_b, len_q, len_k, len_v = q.size(0), q.size(1), k.size(1), v.size(1)
+        len_q, len_k, len_v = q.size(0), k.size(0), v.size(0)
+        sz_b=1
 
         residual = q
 
