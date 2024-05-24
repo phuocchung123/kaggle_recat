5010f61084a3ea8064ac02e76f81b02a571fccd3
phuocchung123 <chungnvp197@gmail.com>
2024-05-25 00:50:49 +0700

    update error to run

--- src_chung/Layers.py
+++ src_chung/Layers.py
@@ -1,7 +1,7 @@
 ''' Define the Layers '''
 import torch.nn as nn
 import torch
-from SubLayers import MultiHeadAttention, PositionwiseFeedForward
+from src_chung.SubLayers import MultiHeadAttention, PositionwiseFeedForward
 
 
 __author__ = "Yu-Hsiang Huang"
