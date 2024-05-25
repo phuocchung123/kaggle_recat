2f190abf8582659134df487c2ce6e2b4f972d5cb
phuocchung123 <chungnvp197@gmail.com>
2024-05-25 15:01:20 +0700

    update epoch =50

--- main_finetune.py
+++ main_finetune.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
         os.makedirs("/kaggle/working/sample/data_chung/model/finetuned/")
 
     start_point=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print('Selfattention,multi_head=8, epoch=20, dim_inner=2048,d_k=64,d_v=64')
+    print('crossattention, numlayer=1,multi_head=8, epoch=20, dim_inner=2048,d_k=64,d_v=64')
     print('commit: modify SubLayer.py because self attention for each reaction not for batch')
 
     finetune(args)
