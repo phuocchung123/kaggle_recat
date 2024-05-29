04041ec9702d2024fa5c3d78a28157043377689c
phuocchung123 <chungnvp197@gmail.com>
2024-05-30 05:09:43 +0700

    update readout_trial

--- src_chung/model.py
+++ src_chung/model.py
@@ -134,7 +134,7 @@ class reactionMPNN(nn.Module):
             nn.Linear(predict_hidden_feats, predict_hidden_feats),
             nn.PReLU(),
             nn.Dropout(prob_dropout),
-            nn.Linear(predict_hidden_feats, 50),
+            nn.Linear(predict_hidden_feats, 115),
         )
 
         # Cross-Attention Module
