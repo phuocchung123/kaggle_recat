b618856eb467e52bb41655a818662e89526de88b
phuocchung123 <chungnvp197@gmail.com>
2024-05-31 07:14:54 +0700

    update

--- main_finetune.py
+++ main_finetune.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
         os.makedirs("/kaggle/working/sample/data_chung/model/finetuned/")
 
     start_point=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print('JUST GIN, reaction_feat=reaction_feat*weight+ reagents*(1-0.1-weight) + 0.1*reaction_mean ,epoch=20')
+    print('50K, JUST GIN, reaction_feat=reaction_feat*weight+ reagents*(1-0.1-weight) + 0.1*reaction_mean ,epoch=20')
 
     finetune(args)
 
